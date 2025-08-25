63cc86f9b849bfa01b90973f913767a3c9f241d2
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-08-25 21:16:13 +0545

    feat: add notes object and show in browser

--- part3/http-module/index.js
+++ part3/http-module/index.js
@@ -1,9 +1,32 @@
 const http = require('http');
 
+let notes = [
+    {
+        id: "1",
+        content: "HTML is easy",
+        important: true
+    },
+    {
+        id: "2",
+        content: "Browser can execute only JavaScript",
+        important: false
+    },
+    {
+        id: "3",
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+    }
+]
+
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, { 'content-type': 'text/plain' });
+//     res.end('Hello World!');
+// });
+
 const app = http.createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hello World!');
-});
+    res.writeHead(200, {'content-type': 'application/json'});
+    res.end(JSON.stringify(notes));
+})
 
 const port = 3001;
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
