2d5df4e0a424f3c3a600b836b34f49146a4be73e
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-08-25 21:33:22 +0545

    feat: add note object and shown to route notes

--- part3/work-in-express/index.js
+++ part3/work-in-express/index.js
@@ -4,10 +4,33 @@ const express = require('express');
 const app = express();
 
 // define a port number
-const port = 3001;
+const port = 3000;
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
 
 app.get('/', (req, res) => {
     res.send('Hello World! I\'m using express framework');
 })
+
+app.get('/api/notes', (req, res) => {
+    res.send(notes);
+    console.log('successfully shown the default notes');
+});
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
